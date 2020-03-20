f45a1940111c974312a98734fafe68056442e752
Cloud User <centos@ip-172-31-20-251.eu-west-2.compute.internal>
2020-03-20 23:25:35 +0000

    s3 with alias

---
+++ s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "oregon" {
+  bucket = "my-supercool-bucket-oregon"
+  provider   = "aws.oregon"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+ resource "aws_s3_bucket" "california" {
+  bucket = "my-supercool-bucket-california"
+  provider   = "aws.california"
+  tags = {
+    Name        = "My bucket"
+    Environment = "qa"
+  }
+}
+
+resource "aws_s3_bucket" "ohio" {
+  bucket = "my-supercool-bucket-ohio"
+  provider   = "aws.ohio"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "virginia" {
+  bucket = "my-supercool-bucket-virginia"
+  provider   = "aws.virginia"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
