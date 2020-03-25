9c92b93a7e1dffb34f1932d03ea01ac0b71b27df
atakan erdil <atakanerdilusa@hotmail.com>
2020-03-25 00:24:46 +0000

    updated s3-terraform-atakan

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-supercool-bucket-state"
-    key    = "path/to/my/key"
+    bucket = "my-erdil-bucket-state"
+    key    = "path/to/my/key/s3.tfvars"
     region = "us-east-1"
     #dynamodb_table = "terraform-class"
   }
